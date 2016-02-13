88682b517c5e4efe36f241fbcf8caaa70e8e0053
Andrey Pushkarev <fealaer@gmail.com>
2016-02-13 16:58:30 +1100

    Add release to fealaer.ru

--- bin/release.sh
+++ bin/release.sh
@@ -22,5 +22,25 @@ git push origin :gh-pages
 # Push new version of build to the gh-pages branch
 git subtree push --prefix dist origin gh-pages
 
+# Check out to release.ru branch
+git checkout release.ru
+
+# Pull all changes from master to release
+git rebase release
+
+echo 'fealaer.ru' > app/CNAME
+
+# Build new version of the application
+gulp build
+
+# Commit changes in dist to the release branch
+git add dist
+git commit -m "\"$COMMENT\""
+
+# Delete old version of branch gh-pages
+git push fealaer.ru :gh-pages
+# Push new version of build to the gh-pages branch
+git subtree push --prefix dist fealaer.ru gh-pages
+
 # Check out to master branch
 git checkout master
