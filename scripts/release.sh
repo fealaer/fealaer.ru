c43b4ddfff0f72966f920b106ee20c52ed83e2aa
Andrey Pushkarev <fealaer@gmail.com>
2016-02-13 16:18:56 +1100

    Add release script

---
+++ scripts/release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Check out to release branch
+git checkout release
+
+# Pull all changes from master to release
+git pull origin master
+
+# Build new version of the application
+gulp build
+
+# Create commit message
+DATE=`date +%Y-%m-%d:%H:%M`
+COMMENT="New version from $DATE"
+
+# Commit changes in dist to the release branch
+git add dist
+git commit -m $COMMENT
+
+# Push new version of build to the gh-pages branch
+git subtree push --prefix dist origin gh-pages
+
+# Check out to master branch
+git checkout master
